ff3aa5aecca37cec84249a1119e3287f05d2f93b
Andrew Nagy <anagy@clearlyip.com>
2023-03-08 20:06:00 +0000

    feat: 🐛 Updates the schema reference
    Updates the schema reference by merging chrome and mozilla types together for the manifest and then generating the manifest that way. Also add tests to test against manifest specific settings
    
    #357

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
   extends: ["airbnb-base", "prettier"],
   parser: "@typescript-eslint/parser",
---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: {
+          resolveJsonModule: true,
+          esModuleInterop: true,
+        },
+      },
+    ],
+  },
+};
